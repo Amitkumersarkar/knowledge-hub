99fce1babf0a2377cd87aa5647c6ca2dfad93333
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-05-22 21:40:13 +0600

    added all file

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-center text-2xl font-bold'>
+        <h2>hello</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
